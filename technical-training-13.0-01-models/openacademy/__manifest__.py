0ece1d632757708298f3637a34fa5eda0076f315
otaner68 <renato.abreu-nunes@laposte.net>
2021-03-03 12:40:02 +0100

    play with orm

--- technical-training-13.0-01-models/openacademy/__manifest__.py
+++ technical-training-13.0-01-models/openacademy/__manifest__.py
@@ -17,13 +17,14 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category':    'OpenAcademy',
-    'version':     '0.7',
+    'version':     '0.9',
 
     # any module necessary for this one to work correctly
-    'depends':     ['base', 'mail'],
+    'depends':     ['base', 'mail', 'product', 'account'],
 
     # always loaded
     'data':        [
+        "security/security.xml",
         "security/ir.model.access.csv",
         "views/course_views.xml",
         "views/session_views.xml",
--- technical-training-13.0-01-models/openacademy/models/course.py
+++ technical-training-13.0-01-models/openacademy/models/course.py
@@ -7,16 +7,25 @@ from odoo import api, exceptions, fields, models
 class Course(models.Model):
     _name = 'openacademy.course'
     _description = 'Course'
+    _inherit = 'mail.thread'
 
     name = fields.Char(name='Title', required=True)
     description = fields.Text()
 
     responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible")
+    can_edit_responsible = fields.Boolean(compute='_compute_can_edit_responsible')
+
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
 
     level = fields.Selection([('1', 'Easy'), ('2', 'Medium'), ('3', 'Hard')], string="Difficulty Level")
     session_count = fields.Integer(compute="_compute_session_count")
     attendee_count = fields.Integer(compute="_compute_attendee_count")
+
+    fname = fields.Char('Filename')
+    datas = fields.Binary('File')
+    currency_id = fields.Many2one('res.currency', 'Currency')
+
+    price = fields.Float('Price')
 
     _sql_constraints = [
         ('name_description_check', 'CHECK(name != description)',
@@ -25,6 +34,10 @@ class Course(models.Model):
         ('name_unique', 'UNIQUE(name)',
          "The course title must be unique"),
     ]
+
+    @api.depends('responsible_id')
+    def _compute_can_edit_responsible(self):
+        self.can_edit_responsible = self.env.user.has_group('openacademy.group_archmaesters')
 
     def copy(self, default=None):
         default = dict(default or {})
@@ -81,11 +94,14 @@ class Session(models.Model):
 
     instructor_id = fields.Many2one('res.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
-    attendee_ids = fields.Many2many('res.partner', string="Attendees", domain="[('is_company', '=', False)]")
+    attendee_ids = fields.Many2many('res.partner', string="Attendees", domain="[('is_company', '=', True)]")
     attendees_count = fields.Integer(compute='_get_attendees_count', store=True)
     seats = fields.Integer()
     taken_seats = fields.Float(compute='_compute_taken_seats', store=True)
     percentage_per_day = fields.Integer("%", default=100)
+
+    is_paid = fields.Boolean('Is paid')
+    product_id = fields.Many2one('product.template', 'Product')
 
     def _warning(self, title, message):
         return {'warning': {
@@ -175,3 +191,26 @@ class Session(models.Model):
         if vals.get('instructor_id'):
             res.message_subscribe([vals['instructor_id']])
         return res
+
+    def create_invoice_teacher(self):
+        teacher_invoice = self.env['account.invoice'].search([
+            ('partner_id', '=', self.instructor_id.id)
+        ], limit=1)
+
+        if not teacher_invoice:
+            teacher_invoice = self.env['account.invoice'].create({
+                'partner_id': self.instructor_id.id,
+            })
+
+        # install module accounting and a chart of account to have at least one expense account in your CoA
+        expense_account = self.env['account.account'].search([('user_type_id', '=', self.env.ref('account.data_account_type_expenses').id)], limit=1)
+        self.env['account.invoice.line'].create({
+            'invoice_id': teacher_invoice.id,
+            'product_id': self.product_id.id,
+            'price_unit': self.product_id.lst_price,
+            'account_id': expense_account.id,
+            'name':       'Session',
+            'quantity':   1,
+        })
+
+        self.write({'is_paid': True})
